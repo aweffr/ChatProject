1f6f1ecb71dd883d08aa4c09b548b5f75a758e75
aweffr <aweffr@qq.com>
2017-06-19 14:45:43 +0800

    把程工的URL配置到开发配置中

--- config.py
+++ config.py
@@ -7,9 +7,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "DAYDAYUP"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    STOMP_URL = "101.37.24.136"
-    STOMP_PORT = 61613
-    STOMP_NAMESPACE = "test2"
     ASYNC_MODE = "gevent"
 
     @staticmethod
@@ -22,6 +19,9 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URI") or \
                               "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
     DROP_AND_CREATE = os.environ.get("DROP_AND_CREATE" or False)
+    STOMP_URL = "101.37.24.136"
+    STOMP_PORT = 61613
+    STOMP_PUBLIC_NAMESPACE = "public"
 
 
 class TestingConfig(Config):
@@ -33,6 +33,7 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
                               "sqlite:///" + os.path.join(basedir, "data.sqlite")
+
 
 config = {
     'development': DevelopmentConfig,
